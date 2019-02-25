76c2c10e137c3cbc020041c7c2ea10bda25407ed
JPEngineer <jalejandro.ingeniero@gmail.com>
2019-02-25 16:11:12 -0300

    Update Service.py

--- JPE_Twitter/bin/service.py
+++ JPE_Twitter/bin/service.py
@@ -2,32 +2,47 @@ import os
 import sys
 import config
 
-# SERVICE START AND STATUS IT'S OKAY
+## CORREGIR LA SALIDA DEL LOG, SE REPITE EL STATUS SERVICE
 def start_service(python, process_name):
     if status_service(process_name):
         pid = os.popen("ps aux | grep " + process_name + " | grep -v 'grep' | awk '{print $2}'").readlines()
         pid = str(pid[0])
-        log.info("The service could not be started because it already exists - PID {0}".format(pid))
+        log.info("Start Service: The service could not be started because it already exists - PID {0}".format(pid))
         return False
     else:
         try:
             os.system(python + " " + process_name + " &")
             pid = os.popen("ps aux | grep " + process_name + " | grep -v 'grep' | awk '{print $2}'").readlines()
             pid = str(pid[0])
-            log.info("The twitter service is started - PID {0}".format(pid))
+            log.info("Start Service: The twitter service is started - PID {0}".format(pid))
             return True
         except Exception as err:
-            log.error("There was an error trying to start the service.")
+            log.error("Start Service: There was an error trying to start the service.")
             log.error(err.args)
             return False
 
 
-def stop_service(param):
-    pass
+def stop_service(process_name):
+    if status_service(process_name):
+        pid = os.popen("ps aux | grep " + process_name + " | grep -v 'grep' | awk '{print $2}'").readlines()
+        pid = str(pid[0])
+        try:
+            os.system("kill -TERM " + pid)
+        except Exception as err:
+            log.error("Stop Service: You can not stop the service, verify that your user has sufficient privileges")
+            log.error(err.args)
+            return False
+        if not status_service(process_name):
+            log.info("Stop Service: The service was successfully stopped")
+            return True
 
 
-def restart_service(param):
-    pass
+def restart_service(python_path, process_name):
+    log.info("========= Restart Service =========")
+    stop_service(process_name)
+    start_service(python_path, process_name)
+    status_service(process_name)
+    log.info("===================================")
 
 
 def status_service(process_name):
@@ -35,10 +50,10 @@ def status_service(process_name):
     if int(process[0]) > 0:
         pid = os.popen("ps aux | grep " + process_name + " | grep -v 'grep' | awk '{print $2}'").readlines()
         pid = pid[0]
-        log.info("Service Status: The twitter service is started - PID {0}".format(pid))
+        log.info("Status Service: The twitter service is started - PID {0}".format(pid))
         return True
     else:
-        log.info("Service Status: The twitter service isn't started")
+        log.info("Status Service: The twitter service isn't started")
         return False
 
 
@@ -48,20 +63,35 @@ process_name = path + "/twitter.py"
 python_path = "/opt/splunk/bin/python"
 log_file = os.path.splitext(os.path.basename(__file__))[0] + '.log'
 
-
 # Log Configurations
 conf_file = config.InitialConfig()
 config_log = config.Log(path + "/logs/" + log_file, __file__)
 log = config_log.log_setup(5)
 
 
+try:
+    # Get parameter
+    param = str(sys.argv[1])
+    if param.lower() == "start":
+        if not start_service(python_path, process_name):
+            exit(1)
+    elif param.lower() == "stop":
+        if not stop_service(process_name):
+            exit(1)
+    elif param.lower() == "status":
+        #if not status_service(process_name):
+        #    log.info("Status Service: The twitter service isn't started")
+        #else:
+        #    log.info("Status Service: The twitter service is started - PID {0}".format(pid))
+        pass
+    elif param.lower() == "restart":
+        if not restart_service(python_path, process_name):
+            exit(1)
+    else:
+        print("Invalid syntax! Remember to use start, stop, restart or status")
 
-
-
-# Get the total number of args passed to the demo.py
-param = str(sys.argv[1])
-print(status_service(process_name))
-
+except Exception as err:
+    log.error(err.args)
 
 #   pid = os.popen("ps aux | grep JPE_Twitter/bin/twitter.py | awk '{print $2}'").readlines()[0] #call pid
 #   print(pid)
